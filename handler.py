660bb6b015c5230df37c5472091fc16c6ccc236a
Brookie Guzder-Williams <bguzder-williams@wri.org>
2017-12-07 17:53:09 -0800

    refactor for more detailed table data. use request params

--- handler.py
+++ handler.py
@@ -3,35 +3,55 @@ import json
 import imageio as io
 from helpers.meanshift import MShift
 import boto3
+import datetime
 
 
 #
 #   AWS CONFIG
 #
-TABLE_NAME='LambdaTest'
+TABLE_NAME='MShiftDEV'
 BUCKET_NAME='lambda-cluster-dev'
+DEFAULT_ZOOM=4
+DEFAULT_WIDTH=15
+DEFAULT_ITERATIONS=25
+DEFAULT_INTENSITY_THRESHOLD=100
+DEFAULT_MIN_COUNT=4
+DEFALUT_WEIGHT_INTENSITY=False
+DEFALUT_DOWNSAMPLE=False
+
 s3 = boto3.client('s3')
 table = boto3.resource('dynamodb').Table(TABLE_NAME)
-
-
+GLAD_START_DATE='20150101'
+RUN_DATE_STR=datetime.datetime.now().strftime("%Y%m%d")
+TIMESTAMP_STR=datetime.datetime.now().strftime("%Y%m%d::%H:%M:%S")
+REQUEST_DICT={
+    'z': DEFAULT_ZOOM,
+    'start': int(GLAD_START_DATE),
+    'end': int(RUN_DATE_STR),
+    'timestamp': TIMESTAMP_STR,
+    'width':int(DEFAULT_WIDTH),
+    'intensity_threshold':DEFAULT_INTENSITY_THRESHOLD,
+    'iterations':DEFAULT_ITERATIONS,
+    'weight_by_intensity': DEFALUT_WEIGHT_INTENSITY,
+    'min_count':DEFAULT_MIN_COUNT,
+    'downsample':DEFALUT_DOWNSAMPLE
+}
 
 #
 # PUBLIC METHODS
 #
 def meanshift(event, context):
-    bucket=event.get('bucket') or BUCKET_NAME
-    file='{}.png'.format(event['file'])
-    download_path='/tmp/{}'.format(file)
-
-    s3.download_file(bucket,file,download_path)
-    out,shape=cluster_data(download_path)
-    item_json=save_out(file,out)
-
+    file, data=_parse_request(event)
+    download_path=_download_data(file,event.get('bucket',BUCKET_NAME))
+    input_data,clusters=cluster_data(download_path,data)
+    data['input_data']=input_data
+    data['clusters']=clusters
+    data['nb_clusters']=len(clusters)
+    save_out(data)
     body = {
         "event": "{}".format(event),
-        "message": "FOUND {} CLUSTERS".format(len(out)),
-        "output": item_json,
-        "shape": shape
+        "message": "FOUND {} CLUSTERS".format(data['nb_clusters']),
+        "table_data": data,
     }
     response = {
         "statusCode": 200,
@@ -43,20 +63,46 @@ def meanshift(event, context):
 #
 # INTERNAL METHODS
 #
-def cluster_data(image_path):
+
+def _parse_request(request):
+    request_dict=dict(REQUEST_DICT,**request)
+    file=_get_file_path(request_dict)
+    request_dict['file']=file
+    request_dict['date_range']='{}-{}'.format(
+            request_dict['start'],
+            request_dict['end'],
+        )
+    return file, request_dict
+
+
+def _download_data(file,bucket):
+    download_path='/tmp/{}'.format(file)
+    s3.download_file(bucket,file,download_path)
+    return download_path
+
+
+def _get_file_path(request):
+    name=request.get('file')
+    if not name:
+        name='{}/{}/{}'.format(
+                request.get('z'),
+                request.get('x'),
+                request.get('y')
+            )
+    return '{}.png'.format(name)
+
+
+def cluster_data(image_path,params):
     im=io.imread(image_path)
-    ms=MShift(im)
-    out=ms.unq(ms.clusters())
-    return out.tolist(), out.shape
+    ms=MShift(im,params)
+    input_data=ms.input_data()
+    clusters=ms.clusters()
+    return input_data.tolist(), clusters.tolist()
 
 
-def save_out(file,out):
-    item_json={
-            'input': file,
-            'points': out
-        }
-    table.put_item(Item=item_json)
-    return item_json
+def save_out(data):
+    table.put_item(Item=data)
+    return data
 
 
 #
--- helpers/meanshift.py
+++ helpers/meanshift.py
@@ -1,57 +1,69 @@
 import math
 import numpy as np
 
-DEFAULT_WIDTH=0.15
-DEFAULT_ITERATIONS=25
+
 INDICES=np.indices((256,256))
-THRESH=100
-MIN_COUNT=4
 
 class MShift(object):
-
-
-    def __init__(self,data,width=DEFAULT_WIDTH):
+    #
+    # PUBLIC METHODS
+    #
+    def __init__(self,data,params):
         self.data=data
-        self.width=width
+        self.width=params['width']/100.0
+        self.min_count=params['min_count']
+        self.threshold=params['intensity_threshold']
+        self.iterations=params['iterations']
+        self._init_properties()
 
 
-    def clusters(self,iterations=DEFAULT_ITERATIONS):
-        self._clusters=np.delete(self.shiftdata(),2,axis=0).reshape(-1,2)
-        for n in range(iterations):
-            for i, x in enumerate(self._clusters):
-                dist = np.sqrt(((x-self._clusters)**2).sum(1))
-                weight = self._gaussian(dist, self.width)
-                self._clusters[i] = (np.expand_dims(weight,1)*self._clusters).sum(0) / weight.sum()
+    def input_data(self):
+        if self._input_data is None:   
+            ijimage=np.apply_along_axis(self._ijdata,0,INDICES)
+            ij_rows=ijimage.reshape(3,-1)
+            zeros=np.all(np.equal(ij_rows, 0),axis=0)
+            self._input_data=ij_rows[:,~zeros]
+        return self._input_data.astype(int)
+
+
+    def clustered_data(self):
+        if self._clusters is None:   
+            self._clusters=np.delete(self.input_data().copy(),2,axis=0).reshape(-1,2)
+            for n in range(self.iterations):
+                for i, x in enumerate(self._clusters):
+                    dist = np.sqrt(((x-self._clusters)**2).sum(1))
+                    weight = self._gaussian(dist)
+                    self._clusters[i] = (np.expand_dims(weight,1)*self._clusters).sum(0) / weight.sum()
         return self._clusters
 
 
-    def unq(self,data):
-        xu,count=np.unique(data,axis=0,return_counts=True)
+    def clusters(self):
+        xu,count=np.unique(self.clustered_data(),axis=0,return_counts=True)
         points=[]
         for t,cnt in zip(xu,count): 
-            if cnt>=MIN_COUNT: 
+            if cnt>=self.min_count: 
                 points.append(t.tolist())
         points=np.array(points)
         return points.astype(int)
 
 
-    def shiftdata(self):
-        ijimage=np.apply_along_axis(self._ijdata,0,INDICES)
-        ij_rows=ijimage.reshape(3,-1)
-        zeros=np.all(np.equal(ij_rows, 0),axis=0)
-        ijvals=ij_rows[:,~zeros]
-        return ijvals
+    #
+    # INTERNAL 
+    #
+    def _init_properties(self):
+        self._input_data=None
+        self._clusters=None
 
 
-    def _gaussian(self,d, bw):
-        return np.exp(-0.5*((d/bw))**2) / (bw*math.sqrt(2*math.pi))
+    def _gaussian(self,d):
+        return np.exp(-0.5*((d/self.width))**2) / (self.width*math.sqrt(2*math.pi))
 
 
     def _ijdata(self,idx):
         i=idx[0]
         j=idx[1]
         val=self.data[i,j]
-        if val>THRESH:
+        if val>self.threshold:
             return [i,j,val]
         else:
             return np.zeros(3)
