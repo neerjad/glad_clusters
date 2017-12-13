4b5abf9eaee3d393faab109a906c44123aa603ca
Brookie Guzder-Williams <bguzder-williams@wri.org>
2017-12-12 16:28:46 -0800

    s3.meta.client

--- helpers/aws.py
+++ helpers/aws.py
@@ -10,7 +10,7 @@ class AWS(object):
         self.s3=S3(bucket)
         self.db=DynamoDB(table_name)
 
-s3_client=boto3.resource('s3')
+
 
 
 class S3(object):
@@ -25,12 +25,11 @@ class S3(object):
 
 
     def download(self,file):
-        client=boto3.resource(self.CLIENT_NAME)
-        self.download_path='{}/{}'.format(self.DOWNLOAD_FOLDER,file)
         if self.ext:
-           self.download_path='{}.{}'.format(self.download_path,self.ext) 
-        print(self.bucket,file,self.download_path)
-        return s3_client.download_file(self.bucket,file,self.download_path)
+            file='{}.{}'.format(file,self.ext)
+        self.download_path='{}/{}'.format(self.DOWNLOAD_FOLDER,file)
+        client=boto3.resource(self.CLIENT_NAME)
+        return client.meta.client.download_file(self.bucket,file,self.download_path)
 
 
 
