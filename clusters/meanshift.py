7cb0bf1305ec01a5f1f8b78d388ef002abd5ff20
Brookie Guzder-Williams <bguzder-williams@wri.org>
2018-01-21 14:24:51 -0800

    return data for 0-255 indices

--- clusters/meanshift.py
+++ clusters/meanshift.py
@@ -38,59 +38,58 @@ class MShift(object):
         self._init_properties()
 
 
-    def centered_data(self):
-        """ add indices (centered at origin) to data
+    def ij_data(self):
+        """ add indices to data
 
             Returns: 
                 array of [i,j,days-since] valued arrays
         """
-        if self._centered_data is None:
-            i=np.subtract(INDICES[0],SHIFT)
-            j=np.subtract(INDICES[1],SHIFT)
-            self._centered_data=np.dstack((i,j,self.data))
-            self._centered_data=self._centered_data.reshape(SIZE**2,-1)
-            self._centered_data=self._centered_data[self._centered_data[:,-1]>0]
-        return self._centered_data
+        self._ij_data=np.dstack((INDICES[0],INDICES[1],self.data))
+        self._ij_data=self._ij_data.reshape(SIZE**2,-1)
+        self._ij_data=self._ij_data[self._ij_data[:,-1]>0]
+        return self._ij_data
 
 
     def clustered_data(self):
         """ shift alert i,j values using mean-shift
-            to final value centroid x,y position
+            to final value centroid x,y position.
+            then shift back and round to get ij coords
 
             Returns: 
-                array of [x,y] valued arrays
+                array of [i,j] valued arrays
         """
         if self._clustered_data is None:   
-            self._clustered_data=self.centered_data()[:,:2].copy()
+            cdata=self.ij_data()[:,:2].copy()
+            cdata=np.subtract(cdata,SHIFT)
             for n in range(self.iterations):
                 if NOISY: 
                     if (n+1)%5==0: print("...{}/{}".format(n+1,self.iterations))
-                for i, x in enumerate(self._clustered_data):
-                    dist=np.sqrt(((x-self._clustered_data)**2).sum(1))
+                for i, x in enumerate(cdata):
+                    dist=np.sqrt(((x-cdata)**2).sum(1))
                     weight=self._gaussian(dist)
-                    self._clustered_data[i]=(
-                        np.expand_dims(weight,1)*self._clustered_data).sum(0)/weight.sum()
-                self._clustered_data=self._clustered_data.round().astype(int)
+                    cdata[i]=(
+                        np.expand_dims(weight,1)*cdata).sum(0)/weight.sum()
+            self._clustered_data=np.add(cdata,SHIFT).round().astype(int)
         return self._clustered_data
 
 
     def clusters(self):
         """ group into clusters
             
-            * groups points at a given x,y
+            * groups points at a given i,j
             * thresholds for nb_pts>min_count
 
             Returns: 
-                array of [x,y,count] valued arrays
+                array of [i,j,count] valued arrays
         """
         if self._clusters is None:   
-            xys,counts=np.unique(
+            points,counts=np.unique(
                 self.clustered_data(),
                 axis=0,
                 return_counts=True)
             counts=np.expand_dims(counts,axis=-1)
             self._clusters=np.concatenate(
-                (xys,counts),
+                (points,counts),
                 axis=-1)
             self._clusters=self._clusters[
                 self._clusters[:,-1]>=self.min_count]
@@ -101,7 +100,7 @@ class MShift(object):
         """ dictionary
         """
         cluster_dict={}
-        cluster_dict['input_data']=self.centered_data()
+        cluster_dict['input_data']=self.ij_data()
         cluster_dict['nb_clusters']=len(self.clusters())
         cluster_dict['clusters']=[
             self.cluster_data(c) for c in self.clusters()]
@@ -111,19 +110,19 @@ class MShift(object):
     def cluster_data(self,cluster):
         """ dictionary
         """
-        x,y,count=cluster
-        ijds=self._ijds_for_xy(x,y)
-        area=ConvexHull(ijds[:,:-1]).area
-        min_date=proc.date_for_days(np.amin(ijds[:,-1]))
-        max_date=proc.date_for_days(np.amax(ijds[:,-1]))
+        i,j,count=cluster
+        alerts=self._alerts_for_points(i,j)
+        area=ConvexHull(alerts[:,:-1]).area
+        min_date=proc.date_for_days(np.amin(alerts[:,-1]))
+        max_date=proc.date_for_days(np.amax(alerts[:,-1]))
         cluster_dict={
-            'x':x,
-            'y':y,
+            'i':i,
+            'j':j,
             'count':count,
             'area':area,
             'max_date':max_date,
             'min_date':min_date,
-            'alerts':ijds }
+            'alerts':alerts }
         return cluster_dict
 
 
@@ -131,7 +130,7 @@ class MShift(object):
     # INTERNAL 
     #
     def _init_properties(self):
-        self._centered_data=None
+        self._ij_data=None
         self._clustered_data=None
         self._joined=None
         self._clusters=None
@@ -140,15 +139,16 @@ class MShift(object):
     def _joined_data(self):
         if self._joined is None:
             self._joined=np.concatenate(
-                (self.clustered_data(),self.centered_data()),axis=-1)
+                (self.clustered_data(),self.ij_data()),axis=-1)
         return self._joined
 
 
-    def _ijds_for_xy(self,x,y):
+    def _alerts_for_points(self,i,j):
         is_in_cluster=np.logical_and(
-            self._joined_data()[:,0]==x,
-            self._joined_data()[:,1]==y)
-        return self._joined_data()[is_in_cluster][:,2:]
+            self._joined_data()[:,0]==i,
+            self._joined_data()[:,1]==j)
+        alerts=self._joined_data()[is_in_cluster][:,2:]
+        return alerts
 
 
     def _gaussian(self,d):
