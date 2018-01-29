6a38636fdfa8a8a9b36b3958cad9e23369f286d1
Brookie Guzder-Williams <bguzder-williams@wri.org>
2018-01-28 17:30:47 -0800

    towards queries

--- utils/service.py
+++ utils/service.py
@@ -1,7 +1,9 @@
+import os
 import math
 import itertools
 import json
 import boto3
+from boto3.dynamodb.conditions import Attr
 import numpy as np
 import pandas as pd
 import utils.multiprocess as mp
@@ -13,9 +15,11 @@ DEFAULT_MIN_COUNT=25
 DEFAULT_WIDTH=5
 DEFAULT_ITERATIONS=25
 DEFAULT_ZOOM=12
+DELETE_RESPONSES=False
+DEFAULT_TABLE=os.environ.get('table')
 LAMBDA_FUNCTION_NAME='gfw-glad-clusters-v1-dev-meanshift'
+
 DATAFRAME_COLUMNS=[
-    'timestamp',
     'count',
     'area',
     'min_date',
@@ -23,6 +27,8 @@ DATAFRAME_COLUMNS=[
     'latitude',
     'longitude',
     'z','x','y','i','j',
+    'file_name',
+    'timestamp',
     'alerts',
     'input_data']
 
@@ -39,7 +45,11 @@ VIEW_COLUMNS=[
 
 
 class ClusterService(object):
-    
+
+
+    #
+    #  PUBLC METHODS
+    #    
     def __init__(self,
             bounds=None,
             tile_bounds=None,
@@ -52,7 +62,8 @@ class ClusterService(object):
             min_count=DEFAULT_MIN_COUNT,
             width=DEFAULT_WIDTH,
             iterations=DEFAULT_ITERATIONS,
-            z=DEFAULT_ZOOM):
+            z=DEFAULT_ZOOM,
+            table=DEFAULT_TABLE):
         self._init_properties()
         self.start_date=DEFAULT_START_DATE
         self.end_date=DEFAULT_END_DATE
@@ -61,52 +72,51 @@ class ClusterService(object):
         self.iterations=iterations
         self.z=z
         self._N=(2**self.z)
+        self.table=table
         self._set_tile_bounds(bounds,tile_bounds,lat,lon,x,y)
-        self.client=boto3.client('lambda')
         
-    
+
+    def fetch(self,key=None,query=None,**kwargs):
+        """ fetch clusters from dynamodb 
+        """
+        db=boto3.resource('dynamodb')
+        print(self.table)
+        table=db.Table(self.table)
+        if key:
+            self.responses=[table.get_item(Key=key)]
+        else:
+            if query or kwargs:
+                filter_expression=self._db_filter(query or kwargs)
+            else:
+                filter_expression=self._build_filter()
+            rows=table.scan(FilterExpression=filter_expression)
+            self.responses=rows.get('Items')
+
+
     def run(self):
         """ find clusters on tiles
         
             NOTE: if (self.x and self.y): 
-                    pass directly to run_tile
+                    pass directly to _run_tile
                   else:
                     use multiprocessing
         """
+        self.lambda_client=boto3.client('lambda')
         if (self.x and self.y):
-            self.responses=[self.run_tile()]
+            self.responses=[self._run_tile()]
         else:
             xys=itertools.product(
                 range(self.x_min,self.x_max+1),
                 range(self.y_min,self.y_max+1))
-            self.responses=mp.map_with_threadpool(self.run_tile,list(xys))
-
-
-    def run_tile(self,location=None,x=None,y=None):
-        """ find clusters on tile
-        
-            NOTE: if no args are passed it will attempt to use 
-                  the x,y (or lat,lon) passed in the constructor
-        
-            Args:
-                location<tuple>: tile-xy value (x,y)
-                x<int>: tile x value
-                y<int>: tile y value
-        """
-        if location: x,y=location
-        if not (x and y):
-            x=self.x
-            y=self.y
-        if (x and y):
-            response=self.client.invoke(
-                FunctionName=LAMBDA_FUNCTION_NAME,
-                InvocationType='RequestResponse',
-                LogType='Tail',
-                Payload=self._request_data(x,y))
-            return self._process_response(response)
+            self.responses=mp.map_with_threadpool(self._run_tile,list(xys))
 
     
     def dataframe(self):
+        """ return data frame of clusters data
+            
+            NOTE: if DELETE_RESPONSES is True
+                  responses json will be removed
+        """
         if  self._dataframe is None:
             self._dataframe=pd.DataFrame(
                 self._dataframe_rows(),
@@ -115,89 +125,96 @@ class ClusterService(object):
                 'timestamp',
                 ascending=False,
                 inplace=True)
+            if DELETE_RESPONSES: self.responses=None
         return self._dataframe
 
 
     def view(self):
+        """ return only VIEW_COLUMNS of .dataframe()
+            * excludes data arrays, i and j, ...
+        """
         return self.dataframe()[VIEW_COLUMNS]
 
 
-    def clusters(self,start=None,end=None):
-        if start or end:
-            responses=self.responses[start:end]
-        elif row_ids is not None:
-            responses=self.responses[start:end]
-        else:
-            responses=self.responses
-            clusters=[]
-            for response in responses:
-                for cluster in response['data']['clusters']:
-                    cinfo=cluster.copy()
-                    cinfo['z']=response['z']
-                    cinfo['x']=response['x']
-                    cinfo['y']=response['y']
-                    clusters.append(cinfo)
-        return clusters  
-
-
-    def tile(self,
+    def cluster(self,
             row_id=None,
-            lat=None, lon=None,
-            z=None, x=None, y=None):
-        """ GET CLUSTER FOR PASSED INFO """
+            lat=None,lon=None,
+            z=None,x=None,y=None,i=None,j=None,
+            timestamp=None,
+            ascending=False,
+            as_view=True):
+        """ fetch cluster data
+
+            Convince method for selecting row of dataframe
+            
+            Args:
+                row_id<int>: dataframe index for cluster
+                lat,lon<floats>: latitude,longitude for cluster
+                z,x,y,i,j<ints>: tile/pixel location for cluster
+                timestamp<str>: timestamp for cluster (consider using row_id)
+                ascending<bool>: 
+                    if true sort by ascending time and grab first matching row
+                as_view:
+                    if true return only VIEW_COLUMNS. 
+                    else include all columns (including input/alerts data)
+        """
         if self._not_none([row_id]):
             row=self.dataframe().iloc[row_id]
-        elif self._not_none([lat,lon]):
-            test=(
-                (self.dataframe().latitude==lat) & 
-                (self.dataframe().longitude==lon))
-            row=self.dataframe()[test].iloc[0]
-        elif self._not_none([x,y,z]):
-            test=(
-                (self.dataframe().z==z) & 
-                (self.dataframe().x==x) & 
-                (self.dataframe().y==y))
-            row=self.dataframe()[test].iloc[0]
-        query={
-            'z': row.z,
-            'x': row.x,
-            'y': row.y }
-        return self._query_responses(query)
+        else:
+            test=True
+            if self._not_none([lat,lon]):
+                test=test & (
+                    (self.dataframe().latitude==lat) & 
+                    (self.dataframe().longitude==lon))
+            elif self._not_none([x,y,z]):
+                test=test & (
+                    (self.dataframe().z==z) & 
+                    (self.dataframe().x==x) & 
+                    (self.dataframe().y==y))
+            if timestamp:
+                test=test & (self.dataframe().timestamp==timestamp)
+            rows=self.dataframe()[test]
+            if ascending: rows.sort_values('timestamp',inplace=True)
+            row=rows.iloc[0]
+        if as_view:
+            return row[VIEW_COLUMNS]
+        else:
+            return row
 
 
-
-    def _dataframe_rows(self):
-        dfrows=[]
-        for response in self.responses:
-            t=response.get('timestamp')
-            z=response.get('z')
-            x=response.get('x')
-            y=response.get('y')
-            for cluster in response['data']['clusters']:
-                i=cluster.get('i')
-                j=cluster.get('j')
-                dfrows.append([
-                        t,
-                        cluster.get('count'),
-                        cluster.get('area'),
-                        cluster.get('min_date'),
-                        cluster.get('max_date'),
-                        self._lat(z,x,y,i,j),
-                        self._lon(z,x,y,i,j),
-                        z,x,y,i,j,
-                        cluster.get('alerts'),
-                        response['data']['input_data']])
-                        
-        return dfrows
-
-
+    #
+    #  INTERNAL METHODS
+    #
     def _init_properties(self):
         self.x=None
         self.y=None
         self._dataframe=None
-        self._clusters=None
 
-    
+
+    def _db_filter(self,query):
+        keys=query.keys()
+        key0=keys.pop()
+        db_filter=Attr(key0).eq(query[key0])
+        for key in keys:
+            db_filter&=Attr(key).eq(query[key])
+        return db_filter
+
+
+    def _build_filter(self):
+        db_filter=Attr('z').eq(self.z)
+        if self.start_date:
+            db_filter &= Attr('start_date').gte(self.start_date)
+        if self.end_date:
+            db_filter &= Attr('end_date').lte(self.end_date)
+        if self.min_count:
+            db_filter &= Attr('min_count').eq(self.min_count)
+        if self.width:
+            db_filter &= Attr('width').eq(self.width)
+        if self.iterations:
+            db_filter &= Attr('iterations').eq(self.iterations)
+        return db_filter
+
+
     def _request_data(self,x,y):
         return json.dumps({
             "z":self.z,
@@ -250,23 +267,56 @@ class ClusterService(object):
         return json.loads(response['Payload'].read())
 
 
-    def _query_responses(self,query):
-        return next(
-            (r for r in self.responses if self._test_response(query,r)),
-            None)
+    def _run_tile(self,location=None,x=None,y=None):
+        """ find clusters on tile
+        
+            NOTE: if no args are passed it will attempt to use 
+                  the x,y (or lat,lon) passed in the constructor
+        
+            Args:
+                location<tuple>: tile-xy value (x,y)
+                x<int>: tile x value
+                y<int>: tile y value
+        """
+        if location: x,y=location
+        if not (x and y):
+            x=self.x
+            y=self.y
+        if (x and y):
+            response=self.lambda_client.invoke(
+                FunctionName=LAMBDA_FUNCTION_NAME,
+                InvocationType='RequestResponse',
+                LogType='Tail',
+                Payload=self._request_data(x,y))
+            return self._process_response(response)
+
+
+    def _dataframe_rows(self):
+        dfrows=[]
+        for response in self.responses:
+            z=response.get('z')
+            x=response.get('x')
+            y=response.get('y')
+            for cluster in response.get('data',{}).get('clusters',[]):
+                i=cluster.get('i')
+                j=cluster.get('j')
+                dfrows.append([
+                        cluster.get('count'),
+                        cluster.get('area'),
+                        cluster.get('min_date'),
+                        cluster.get('max_date'),
+                        self._lat(z,x,y,i,j),
+                        self._lon(z,x,y,i,j),
+                        z,x,y,i,j,
+                        response['file_name'],
+                        response['timestamp'],
+                        np.array(cluster.get('alerts')).astype(int),
+                        np.array(response['data']['input_data']).astype(int)])
+        return dfrows
 
 
     def _not_none(self,values):
         test=[ (val is not None) for val in values ]
         return np.prod(test).astype(bool)
-
-
-    def _test_response(self,query,response):
-        test=[ query[key]==response[key] for key in query.keys()]
-        return np.prod(test).astype(bool)
-
-
-
-
 
 
